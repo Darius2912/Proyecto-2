b645da1cddcee4cf1bc3c068462d4e062933c64a
Jeanrva8 <jretanav@ucenfotec.ac.cr>
2026-04-09 19:53:29 -0600

    archivos rebuild

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c54fcbe6d251a452e3427bf9046c0aa712e4b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2291ae57e23ec06f8551b4a0d8cf1f766f5bae15")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
