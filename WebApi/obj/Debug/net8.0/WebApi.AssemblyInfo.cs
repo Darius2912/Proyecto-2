e8e6194a439fda2b2645d877ee312dcc85c750a0
Jeanrva8 <jretanav@ucenfotec.ac.cr>
2026-04-22 19:57:52 -0600

    correccion en base de datos para almacerna datos de ubicacion separados

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b31c00eee3f853306ca621400aa5ff4ff7a8b387")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87572365fd4dfc76bb1194c408f4824d7849e01b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
